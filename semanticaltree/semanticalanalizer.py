5b74a6d741f88e4152c4e2b15aebd5b4cb35d914
ChuykoMihail <Kirito1mechnik>
2022-01-27 16:22:28 +1000

    semantical analizator added

--- semanticaltree/semanticalanalizer.py
+++ semanticaltree/semanticalanalizer.py
@@ -11,58 +11,38 @@ class SemanticalAnalyzer:
         self.variables = []
         self.scan(self.root)
 
+
+    def checkvar(self, varptr):
+        for var in self.variables:
+            if varptr.value == var[0]:
+                return var
+
+    def checkvarbool(self, varptr):
+        for var in self.variables:
+            if varptr.value == var[0]:
+                return True
+        else: return False
+
     def scan(self, ptr: NodeStruct):
-        self.scantypes(ptr)
-        self.scanvars(ptr)
-
-    def scanvars(self, ptr = None):
-        if ptr.name !="VARIABLE":
+        if ptr.name not in ["ASSIGNMENT"]:
             for child in ptr.childs:
-                self.scanvars(child)
-        elif ptr.prev.name == "ASSIGNMENT":
-            if ptr.prev.childs[0].value == ptr.value:
-                if ptr.prev.childs[1].name in [
-                    "INTEGER", "STRING"
-                ]:
-                    self.variables.append((ptr.value, ptr.prev.childs[1].name))
-                elif ptr.prev.childs[1].name == "VARIABLE":
-                    for var in self.variables:
-                        if ptr.prev.childs[1].value==var[0]:
-                            self.variables.append((ptr.value, var[1]))
-            elif ptr.value not in self.variables[:][0]:
-                sys.stderr.write('Unresolved reference: %s\n' % ptr.value)
-                sys.exit(1)
-
-
-           # if child.name == "VARIABLE":
-           #     if ptr.name =="ASSIGNMENT" and ptr.childs[0].value == child.value:
-
-
-
-    def scantypes(self, ptr: NodeStruct):
-        for child in ptr.childs:
-            if (child.name not in ["OPERATOR"]):
-                self.scantypes(child)
-            else:
-                left = child.childs[0]
-                right = child.childs[1]
-                if (left.name, right.name) in [
-                    ("INTEGER", "STRING"),
-                    ("STRING", "INTEGER")
-                ]:
-                    sys.stderr.write('Illegal type: %s\n' % right.name)
-                    sys.exit(1)
-                elif (left.name, right.name) in [
-                    ("INTEGER", "INTEGER"),
-                    ("STRING", "STRING")
-                ]:
-                    child.name = left.name
-                elif left.name == "OPERATOR":
-                    self.subscantypes(left)
-                    self.scantypes(child.prev)
-                elif right.name == "OPERATOR":
+                self.scan(child)
+        else:
+            left = ptr.childs[0]
+            right = ptr.childs[1]
+            if left.name == "VARIABLE":
+                if right.name == "OPERATOR":
                     self.subscantypes(right)
-                    self.scantypes(child.prev)
+                elif right.name in ["INTEGER", "BOOL", "STRING"]:
+                    self.variables.append((left.value, right.name))
+                elif right.name == "VARIABLE":
+                    if self.checkvarbool(right):
+                        var = self.checkvar(right)
+                        self.variables.append((left.value, var[1]))
+                    else:
+                        sys.stderr.write('Unresolved variable: %s\n' % right.value)
+                        sys.exit(1)
+
 
 
     def subscantypes(self, operation):
@@ -83,3 +63,37 @@ class SemanticalAnalyzer:
             self.subscan(left)
         elif right.name == "OPERATOR":
             self.subscan(right)
+        if left.name == "VARIABLE":
+            if self.checkvarbool(left):
+                var = self.checkvar(left)
+                if (var[1], right.name) in [
+                    ("INTEGER", "INTEGER"),
+                    ("STRING", "STRING")
+                ]:
+                    operation.name = var[1]
+                elif (var[1], right.name) in [
+                    ("INTEGER", "STRING"),
+                    ("STRING", "INTEGER")
+                ]:
+                    sys.stderr.write('Illegal type: %s\n' % right.name)
+                    sys.exit(1)
+            else:
+                sys.stderr.write('Unresolved variable: %s\n' % left.value)
+                sys.exit(1)
+        elif right.name == "VARIABLE":
+            if self.checkvarbool(right):
+                var = self.checkvar(right)
+                if (var[1], left.name) in [
+                    ("INTEGER", "INTEGER"),
+                    ("STRING", "STRING")
+                ]:
+                    operation.name = var[1]
+                elif (var[1], left.name) in [
+                    ("INTEGER", "STRING"),
+                    ("STRING", "INTEGER")
+                ]:
+                    sys.stderr.write('Illegal type: %s\n' % right.name)
+                    sys.exit(1)
+            else:
+                sys.stderr.write('Unresolved variable: %s\n' % left.value)
+                sys.exit(1)
